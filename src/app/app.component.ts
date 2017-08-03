f6dddfeda6331969c07f4c6ba3ea22d5119791c6
徐文清-前端开发组 <xuwq@jpush.cn>
2017-08-03 17:17:31 +0800

    sdk2.3.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { appAction } from './actions';
 import { global } from './services/common/global';
-import '../assets/static/js/jmessage-sdk-web.2.3.0.1.min.js';
+import '../assets/static/js/jmessage-sdk-web.2.3.0.min.js';
 declare function JMessage(obj ?: Object): void;
 
 @Component({
